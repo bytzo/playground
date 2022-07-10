081d54752a2e6a3e36f661a86e91afe32149f932
Bytzo <bytzo@bytzo.net>
2022-07-10 09:57:25 -0700

    The Rust Programming Language: Hello, Cargo!

---
+++ playgrounds/rust-book/hello_cargo/src/main.rs
@@ -0,0 +1,6 @@
+// Rust files should be placed in the src/ directory so Cargo can find them.
+// Documentation files should be placed outside of the src directory.
+
+fn main() {
+    println!("Hello, world!");
+}
